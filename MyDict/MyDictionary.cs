ee00fea6eb99723ee7d8f9966babf64dcee2a2b6
anıl pekin <anlpek1n@gmail.com>
2021-11-12 21:27:37 +0300

    Add project files.

---
+++ MyDict/MyDictionary.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDict
+{
+    class MyDictionary<T,S>
+    {
+        T[] keys;
+        S[] values;
+        T[] tempKey;
+        S[] tempValue;
+        public MyDictionary()
+        {
+            keys = new T[0];
+            values = new S[0];
+        }
+
+        public void Add(T key, S value)
+        {
+            tempKey = keys;
+            tempValue = values;
+            keys = new T[keys.Length + 1];
+            values = new S[values.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+                values[i] = tempValue[i];
+            }
+            keys[keys.Length - 1] = key;
+            values[values.Length - 1] = value;
+            Console.WriteLine("Added.");
+        }
+    }
+}
---
+++ MyDict/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+
+namespace MyDict
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            MyDictionary<int, string> myDictionary = new MyDictionary<int, string>();
+
+            myDictionary.Add(1, "Anıl");
+
+
+        }
+    }
+}
